5bdb4c00ac989526c9d189049e43d9bd00c846d7
Vincent Nathanael <nathanaellvincent@gmail.com>
2026-04-05 14:35:00 +0100

    Bootstrap Spring Boot application entry point

---
+++ src/main/java/com/nathanaellvincent/riskguard/RiskguardApplication.java
@@ -0,0 +1,11 @@
+package com.nathanaellvincent.riskguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskguardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RiskguardApplication.class, args);
+    }
+}
